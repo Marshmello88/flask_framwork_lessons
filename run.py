a2bb264453fc2d35f390a331cc16af206ea6491f
Marshmello88 <m.neibauere@gmail.com>
2020-05-13 19:27:41 +0000

    form added

--- run.py
+++ run.py
@@ -1,25 +1,48 @@
   
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
 
-@app.route("/")
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
-
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 if __name__ == '__main__':
